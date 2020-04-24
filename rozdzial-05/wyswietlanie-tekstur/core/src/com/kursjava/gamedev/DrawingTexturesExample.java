eadae3b97ab1b29e470746c94ac0b41d14788b0d
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-04-24 20:52:50 +0200

    Rozdzial 5: rysowanie tekstury

--- rozdzial-05/wyswietlanie-tekstur/core/src/com/kursjava/gamedev/DrawingTexturesExample.java
+++ rozdzial-05/wyswietlanie-tekstur/core/src/com/kursjava/gamedev/DrawingTexturesExample.java
@@ -7,27 +7,32 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DrawingTexturesExample extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+  public static final int SCREEN_WIDTH = 500;
+  public static final int SCREEN_HEIGHT = 400;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+  private SpriteBatch batch;
+  private Texture catImg, winkingCatImg;
+
+  @Override
+  public void create () {
+    batch = new SpriteBatch();
+    catImg = new Texture("cat.png");
+    winkingCatImg = new Texture("winking_cat.png");
+  }
+
+  @Override
+  public void render () {
+    Gdx.gl.glClearColor(0.741f, 0.874f, 0.976f, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    batch.begin();
+    batch.draw(catImg, 200, 100);
+    batch.end();
+  }
+
+  @Override
+  public void dispose () {
+    batch.dispose();
+    catImg.dispose();
+    winkingCatImg.dispose();
+  }
 }
--- rozdzial-05/wyswietlanie-tekstur/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
+++ rozdzial-05/wyswietlanie-tekstur/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kursjava.gamedev.DrawingTexturesExample;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config =
-				new LwjglApplicationConfiguration();
+  public static void main (String[] arg) {
+    LwjglApplicationConfiguration config =
+        new LwjglApplicationConfiguration();
 
-		config.resizable = false;
-		config.width = 800;
-		config.height = 640;
-		config.title = "Wyświetlanie tekstur - https://kursjava.com";
+    config.resizable = false;
+    config.width = 500;
+    config.height = 400;
+    config.title = "Rysowanie tekstur - https://kursjava.com";
 
-		new LwjglApplication(new DrawingTexturesExample(), config);
-	}
+    new LwjglApplication(new DrawingTexturesExample(), config);
+  }
 }
