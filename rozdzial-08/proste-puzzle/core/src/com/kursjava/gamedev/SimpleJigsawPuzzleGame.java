08e9c44a2c8606b4da49c2b18950d0fab7e97743
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-05-04 16:05:25 +0200

    Puzzle: wczytywanie obrazu i ustawienie rozmiaru okna

--- rozdzial-08/proste-puzzle/core/src/com/kursjava/gamedev/SimpleJigsawPuzzleGame.java
+++ rozdzial-08/proste-puzzle/core/src/com/kursjava/gamedev/SimpleJigsawPuzzleGame.java
@@ -3,13 +3,24 @@ package com.kursjava.gamedev;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SimpleJigsawPuzzleGame extends ApplicationAdapter {
+  public static final int WINDOW_WIDTH = 800;
+  public static final int WINDOW_HEIGHT = 450;
+
+  private Texture puzzleImg;
   private SpriteBatch batch;
+  private int puzzleOriginX;
+  private int puzzleOriginY;
 
   @Override
   public void create () {
+    puzzleImg = new Texture("kitty.png");
+    puzzleOriginX = WINDOW_WIDTH / 2 - puzzleImg.getWidth() / 2;
+    puzzleOriginY = WINDOW_HEIGHT / 2 - puzzleImg.getHeight() / 2;
+
     batch = new SpriteBatch();
   }
 
@@ -17,10 +28,15 @@ public class SimpleJigsawPuzzleGame extends ApplicationAdapter {
   public void render () {
     Gdx.gl.glClearColor(1, 1, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    batch.begin();
+    batch.draw(puzzleImg, puzzleOriginX, puzzleOriginY);
+    batch.end();
   }
 
   @Override
   public void dispose () {
     batch.dispose();
+    puzzleImg.dispose();
   }
 }
--- rozdzial-08/proste-puzzle/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
+++ rozdzial-08/proste-puzzle/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
@@ -6,7 +6,14 @@ import com.kursjava.gamedev.SimpleJigsawPuzzleGame;
 
 public class DesktopLauncher {
   public static void main (String[] arg) {
-    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    LwjglApplicationConfiguration config =
+        new LwjglApplicationConfiguration();
+
+    config.resizable = false;
+    config.width = SimpleJigsawPuzzleGame.WINDOW_WIDTH;
+    config.height = SimpleJigsawPuzzleGame.WINDOW_HEIGHT;
+    config.title = "Simple Puzzle Game - https://kursjava.com";
+
     new LwjglApplication(new SimpleJigsawPuzzleGame(), config);
   }
 }
