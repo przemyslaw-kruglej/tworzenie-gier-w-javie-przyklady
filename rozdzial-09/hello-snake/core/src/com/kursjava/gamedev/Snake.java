041395a68b00dbb29fa4db16d62dab5cf04e3d39
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-06-10 18:33:30 +0200

    Hello Snake: obsługa wychodzenia poza krawędzie okna

--- rozdzial-09/hello-snake/core/src/com/kursjava/gamedev/Snake.java
+++ rozdzial-09/hello-snake/core/src/com/kursjava/gamedev/Snake.java
@@ -70,23 +70,33 @@ public class Snake {
   }
 
   private void move() {
+    // przesuń wszystkie segmenty poza głową
     for (int i = snakeSegments.size() - 1; i > 0; i--) {
       snakeSegments.get(i).set(snakeSegments.get(i - 1));
     }
 
+    // przesuń głowę
+    int segmentWidth = texture.getWidth();
+    int segmentHeight = texture.getWidth();
+
+    // pozycje X, Y ostatniego segmentu przed górną i prawą krawędzią okna
+    int lastWindowSegmentX = Gdx.graphics.getWidth() - segmentWidth;
+    int lastWindowSegmentY = Gdx.graphics.getHeight() - segmentHeight;
+
     GridPoint2 head = snakeSegments.get(0);
+
     switch (direction) {
       case LEFT:
-        head.x -= texture.getWidth();
+        head.x = (head.x == 0) ? lastWindowSegmentX : head.x - segmentWidth;
         break;
       case UP:
-        head.y += texture.getHeight();
+        head.y = (head.y == lastWindowSegmentY) ? 0 : head.y + segmentHeight;
         break;
       case RIGHT:
-        head.x += texture.getWidth();
+        head.x = (head.x == lastWindowSegmentX) ? 0 : head.x + segmentWidth;
         break;
       case DOWN:
-        head.y -= texture.getHeight();
+        head.y = (head.y == 0) ? lastWindowSegmentY : head.y - segmentHeight;
         break;
     }
   }
