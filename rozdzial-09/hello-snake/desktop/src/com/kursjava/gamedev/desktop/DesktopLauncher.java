02d53c3e7579d84316ef5664596c44dacdcce73a
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-06-20 11:45:44 +0200

    Hello Snake: ustawienie docelowego rozmiaru okna

--- rozdzial-09/hello-snake/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
+++ rozdzial-09/hello-snake/desktop/src/com/kursjava/gamedev/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.kursjava.gamedev.HelloSnake;
 public class DesktopLauncher {
   public static void main (String[] arg) {
     LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+    config.resizable = false;
+    config.width = 420;
+    config.height = 225;
+    config.title = "Hello Snake! https://kursjava.com";
+
     new LwjglApplication(new HelloSnake(), config);
   }
 }
